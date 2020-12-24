c318062aa1be16bcfea46d25c3e7c996f8290abb
Parv1304 <75790981+Parv1304@users.noreply.github.com>
2020-12-24 15:33:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var car,wall;
+var speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+  speed=Math.round(random(55,90));
+  weight=Math.round(random(400,1500));
+  car=createSprite(50,200,50,50);
+  wall=createSprite(1500,200,60,height/2);
+  wall.shapeColor="white";
+  car.velocityX=speed;
+}
+
+function draw() {
+  background(0,0,0);
+  if(wall.x-car.x<car.width/2+wall.width/2)
+  {
+    car.velocityX=0;
+    var def=(0.5*weight*speed*speed)/22500;
+    if(def<100)
+    {
+      car.shapeColor="green";
+    }
+    if(def>=100&&def<=180)
+    {
+      car.shapeColor="yellow";
+    }
+    if(def>180)
+    {
+      car.shapeColor="red";
+    }
+  }
+  //Text("Deformation:"+def,800,300);
+  drawSprites();
+
+}
